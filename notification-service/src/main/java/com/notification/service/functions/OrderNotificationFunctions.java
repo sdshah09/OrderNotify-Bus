e24c81d7466ca37b3a0a6e7b63b1bd09cb8a4739
Shaswat Shah <99328539+sdshah09@users.noreply.github.com>
2025-07-15 22:01:23 -0500

    feat: create consumer for notification service to receive events

---
+++ notification-service/src/main/java/com/notification/service/functions/OrderNotificationFunctions.java
@@ -0,0 +1,28 @@
+package com.notification.service.functions;
+
+import com.notification.service.dto.OrderDetail;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class OrderNotificationFunctions {
+
+    @Bean
+    public Function<OrderDetail,String> orderEventReceiver() {
+        return (orderDetail -> {
+            //process
+            System.out.println("Sending noitifcation to user");
+            logicToSendEmailAndMessageToUser(orderDetail.getEmail(), orderDetail.getUserPhone());
+            return "order notification send to user";
+        });
+    }
+
+    private void logicToSendEmailAndMessageToUser(String email, String userPhone) {
+        System.out.println("Sending email to: " + email);
+        System.out.println("Sending text to: " + userPhone);
+        System.out.println("Notification sent to user");
+        System.out.println("-------------------------");
+    }
+}
